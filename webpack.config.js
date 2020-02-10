66cdba679de90dd41ee969b00ee9e142b5063d48
Marusya Ganza <maursyaganza@yandex.ru>
2020-02-10 21:22:25 +0300

    refactor build-utils

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const modeConfig = env => require(`./build-utils/mode-configs/webpack.${env}`)(env);
 const webpackMerge = require('webpack-merge');
 const loadPresets = require('./build-utils/load-presets');
 
 module.exports = ({mode, presets} = {mode: 'production', presets: []}) => {
-    console.log('env', {presets});
     return webpackMerge({
         mode,
         output: {
