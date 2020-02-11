d0318e6a0c77534d71b20f318fc058f3da22ffb0
Marusya Ganza <maursyaganza@yandex.ru>
2020-02-12 00:05:30 +0300

    react minimum configuration

--- src/img.js
+++
@@ -1,8 +0,0 @@
-import src from './assets/sample.jpg';
-const img = document.createElement('img');
-
-img.src = src;
-img.height = '100';
-img.width = '100';
-
-export {img};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import {nav} from "./nav";
-
-var loadImage = () => import("./img");
-import {button} from "./button";
-
-document.body.appendChild(nav);
-document.body.appendChild(button);
-button.addEventListener('click', event => {
-    loadImage().then(m =>
-        document.body.appendChild(m.img)
-)
-    ;
-});
-
+// TODO place something meaningful here
+console.log('just a temporary entry file');
