8892c5dbb15ab6c35a2daa81a0d97e85dd3a6775
Marusya Ganza <maursyaganza@yandex.ru>
2021-01-16 19:09:15 +0300

    fix(all): LP-74 fix various bug

--- src/app.js
+++ src/app.js
@@ -13,29 +13,31 @@ const Home = lazy(() => import('./pages/home-page'));
 const NotFoundPage = lazy(() => import('./pages/not-found-page'));
 const GamesPage = lazy(() => import('./pages/games-page'));
 const CardPage = lazy(() => import('./pages/word-cards-page'));
-const SearchPage = lazy(() => import('./pages/search-page'));
 const EditWordPage = lazy(() => import('./pages/edit-word-card-page'));
+const SearchPage = lazy(() => import('./pages/search-page'));
 
 const navItems = Object.values(PATHS);
 
 export const App = () => (
   <ErrorBuondary>
     <AppProvider>
-      <div className={styles.page}>
-        <Header navItems={navItems} />
-        <main className={styles.main}>
-          <Suspense fallback={<Spinner />}>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/word_games" component={GamesPage} />
-              <Route path="/word_cards" component={CardPage} />
-              <Route path="/search_words" component={SearchPage} />
-              <Route path="/words/edit/:id" component={EditWordPage} />
-              <Route path="*" component={NotFoundPage} />
-            </Switch>
-          </Suspense>
-        </main>
-        <Footer />
+      <div className={styles.container}>
+        <div className={styles.page}>
+          <Header navItems={navItems} />
+          <main className={styles.main}>
+            <Suspense fallback={<Spinner />}>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/word_games" component={GamesPage} />
+                <Route path="/word_cards/:id" component={EditWordPage} />
+                <Route path="/word_cards" component={CardPage} />
+                <Route path="/search_words" component={SearchPage} />
+                <Route path="*" component={NotFoundPage} />
+              </Switch>
+            </Suspense>
+          </main>
+          <Footer />
+        </div>
       </div>
     </AppProvider>
   </ErrorBuondary>
